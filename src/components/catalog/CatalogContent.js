6433171910e0b6cc5210672d1b990ec602186ce8
jeff <jeff@jeffHome.lan>
2018-06-30 10:48:36 +0200

    catalog datable wip

--- src/components/catalog/CatalogContent.js
+++ src/components/catalog/CatalogContent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import AudioPlayer from './Audioplayer'
+import CatalogTable from './CatalogTable'
 
 export default class CatalogContent extends Component {
   render() {
@@ -7,32 +8,10 @@ export default class CatalogContent extends Component {
       <div>
         <h3>Most Popular Royalty Free Music</h3>
         <h5>200 tracks</h5>
-        <table className="table table-dark">
-          <thead>
-            <tr>
-              <th scope="col"></th>
-              <th scope="col">Title</th>
-              <th scope="col">Length</th>
-              <th scope="col">Shorts(sec)</th>
-              <th scope="col">Loops</th>
-              <th scope="col"></th>
-              <th scope="col"></th>
-            </tr>
-          </thead>
-          <tbody>
-
-            <tr>
-              <th><AudioPlayer /></th>
-              <td>My song number</td>
-              <td>2:00</td>
-              <td>150</td>
-              <td></td>
-              <td></td>
-              <td><button type="button" className="btn btn-warning">Buy 25€</button></td>
-            </tr>
-            
-          </tbody>
-        </table>
+       
+         
+        <button type="button" className="btn btn-warning">Buy 25€</button>
+        <CatalogTable />
       </div>
     )
   }
---
+++ src/components/catalog/CatalogTable.js
@@ -0,0 +1,88 @@
+import React, {Component} from "react";
+import { render } from "react-dom";
+import { makeData, Logo, Tips } from "./Utils";
+
+// Import React Table
+import ReactTable from "react-table";
+import "react-table/react-table.css";
+
+class CatalogTable extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: makeData()
+    };
+  }
+  render() {
+    const { data } = this.state;1
+    return (
+      <div>
+        <ReactTable
+          data={data}
+          columns={[
+            {
+              Header: "Name",
+              columns: [
+                {
+                  Header: "First Name",
+                  accessor: "firstName",
+                  Footer: () =>
+                    <div style={{ textAlign: "center" }}>First Name</div>
+                },
+                {
+                  Header: "Last Name",
+                  accessor: "lastName",
+                  Footer: () =>
+                    <div style={{ textAlign: "center" }}>Last Name</div>
+                }
+              ]
+            },
+            {
+              Header: "Info",
+              columns: [
+                {
+                  Header: "Age",
+                  accessor: "age",
+                  Footer: () => <div style={{ textAlign: "center" }}>Age</div>
+                }
+              ]
+            },
+            {
+              Header: "Expand",
+              columns: [
+                {
+                  expander: true,
+                  Header: () => <strong>More</strong>,
+                  width: 65,
+                  Expander: ({ isExpanded, ...rest }) =>
+                    <div>
+                      {isExpanded
+                        ? <span>&#x2299;</span>
+                        : <span>&#x2295;</span>}
+                    </div>,
+                  style: {
+                    cursor: "pointer",
+                    fontSize: 25,
+                    padding: "0",
+                    textAlign: "center",
+                    userSelect: "none"
+                  },
+                  Footer: () => <span>&hearts;</span>
+                }
+              ]
+            }
+          ]}
+          defaultPageSize={10}
+          className="-striped -highlight"
+          SubComponent={() => <div style={{padding: '10px'}}>Hello</div>}
+        />
+        <br />
+       
+      </div>
+    );
+  }
+}
+
+export default CatalogTable
+
+

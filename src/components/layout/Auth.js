ab9822f763c4c10d66022d80333a779c4d71892e
jeff <jfroussel75@gmail.com>
2018-07-09 08:30:48 +0200

    wip

--- src/components/layout/Auth.js
+++ src/components/layout/Auth.js
@@ -3,6 +3,18 @@ import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import firebase from 'firebase';
 import './Auth.css'
 
+const style = {
+    welcomeContainer: {
+        borderRight: 'solid 2px #C7C7C7'
+    },
+    welcomeCard: {
+        border: 'solid 1px C7C7C7'
+    },
+    connectContainer: {
+
+    },
+
+}
 
 class Auth extends React.Component {
 
@@ -20,7 +32,7 @@ class Auth extends React.Component {
             firebase.auth.GithubAuthProvider.PROVIDER_ID,
             firebase.auth.TwitterAuthProvider.PROVIDER_ID,
             firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-            
+
         ],
         callbacks: {
             signInSuccess: () => false
@@ -40,11 +52,35 @@ class Auth extends React.Component {
     render() {
         if (!this.state.isSignedIn) {
             return (
-                <div className="pt-5">
-                    <h2 className="font-weight-light">Sign in or Sign up</h2>
-                    <p className="lead constrain-width mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. At aliquam rhoncus donec magna turpis, dictum sit amet tellus at, commodo elementum sapien.</p>
-                            
-                    <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={firebase.auth()} />
+                <div className="row pt-5">
+                    <div className="col-6 text-center" style={style.welcomeContainer}>
+                        <h2 className="font-weight-light">Welcome</h2>
+                        <p className="lead constrain-width mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. At aliquam rhoncus donec magna turpis, dictum sit amet tellus at, commodo elementum sapien.</p>
+                        <div className="row">
+                            <div className="col-6" style={style.welcomeCard}>
+                                <i class="fab fa-dribbble fa-5x mb-5"></i>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. At aliquam rhoncus donec magna turpis, dictum sit amet tellus at, commodo elementum</p>
+                            </div>
+                            <div className="col-6" style={style.welcomeCard}>
+                                <i class="fab fa-dribbble fa-5x mb-5"></i>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. At aliquam rhoncus donec magna turpis, dictum sit amet tellus at, commodo elementum</p>
+                            </div>                        </div>
+                        <div className="row">
+                            <div className="col-6" style={style.welcomeCard}>
+                                <i class="fab fa-dribbble fa-5x mb-5"></i>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. At aliquam rhoncus donec magna turpis, dictum sit amet tellus at, commodo elementum</p>
+                            </div>
+                            <div className="col-6" style={style.welcomeCard}>
+                                <i class="fab fa-dribbble fa-5x mb-5"></i>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. At aliquam rhoncus donec magna turpis, dictum sit amet tellus at, commodo elementum</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-6 text-center" style={style.connectContainer}>
+                        <h2 className="font-weight-light">Sign in or Sign up</h2>
+                        <p className="lead constrain-width mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. At aliquam rhoncus donec magna turpis, dictum sit amet tellus at, commodo elementum sapien.</p>
+                        <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={firebase.auth()} />
+                    </div>
                 </div>
             );
         }

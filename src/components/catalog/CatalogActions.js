e03aacca63785f6e6edab6c3a03f719b7d5fad34
jeff <jfroussel75@gmail.com>
2018-09-06 20:50:38 +0200

    wip

--- src/components/catalog/CatalogActions.js
+++ src/components/catalog/CatalogActions.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Modal from 'react-responsive-modal';
 
 const style = {
     icon: {
@@ -21,14 +20,16 @@ const style = {
 
 
 class CatalogActions extends Component {
-    state = {
-        openModal: false,
-    };
 
-    onOpenModal = () => {
-        //this.setState({ open: true });
-        alert('test')
-    };
+    constructor(props) {
+        super(props)
+        this.state = {
+            
+        }
+    }
+    
+
+   
 
     onCloseModal = () => {
         this.setState({ open: false });
@@ -40,7 +41,7 @@ class CatalogActions extends Component {
             <div>
                 <div className="row">
                     <div style={style.iconBox} className="ml-3" >
-                        <i className="far fa-heart" style={style.icon} onClick={this.onOpenModal}  ></i>
+                        <i className="far fa-heart" style={style.icon}  ></i>
                     </div>
                     <div style={style.iconBox} className="ml-2">
                         <i className="fas fa-download" style={style.icon} ></i>
@@ -48,26 +49,10 @@ class CatalogActions extends Component {
                     <div style={style.iconBox} className="ml-2">
                         <i className="fas fa-music" style={style.icon} ></i>
                     </div>
+                    
                     <div className="example">
 
-                        <Modal
-                            open={openModal}
-                            onClose={this.onCloseModal}
-                            center
-                            classNames={{
-                                transitionEnter: 'transition-enter',
-                                transitionEnterActive: 'transition-enter-active',
-                                transitionExit: 'transition-exit-active',
-                                transitionExitActive: 'transition-exit-active',
-                            }}
-                            animationDuration={1000}
-                        >
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                                pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
-                                hendrerit risus, sed porttitor quam.
-                            </p>
-                        </Modal>
+                       
                     </div>
                 </div>
 
